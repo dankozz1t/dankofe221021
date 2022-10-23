e7170eee6836d963664e2af4a19a5b7deabec832
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-10-23 17:54:52 +0300

    fix phone and add styles

--- src/shared/components/UserInfo/UserInfo.jsx
+++ src/shared/components/UserInfo/UserInfo.jsx
@@ -2,32 +2,40 @@ import React from "react";
 import Button from "shared/components/Button";
 import UserAvatar from "shared/components/UserAvatar";
 
+import { ReactComponent as IconClose } from "assets/icons/close.svg";
+
 import s from "./UserInfo.module.css";
 
 const UserInfo = ({ user, onViewUserInfo }) => {
   const { phone, name, nickname, email, position, photo } = user;
 
-  const userPhoto = `assets/images/${photo}`;
+  const userPhoto = `images/${photo}`;
   const userEmail = `mailto:${email}`;
+  const userPhone = `tel:${phone}`;
 
   return (
     <div className={s.box}>
-      <Button onClick={onViewUserInfo}>X</Button>
-      <UserAvatar src={userPhoto} alt={nickname} width="50" height="50" />
+      <button className={s.button} onClick={onViewUserInfo}>
+        <IconClose />
+      </button>
+      <UserAvatar src={userPhoto} alt={nickname} size="80px" />
       <h2>{name}</h2>
-      <h3>{position}</h3>
+      <h3 className={s.position}>{position}</h3>
 
       <div className={s.contactBox}>
-        <p>Phone</p>
-        <p>{phone}</p>
-      </div>
+        <div className={s.contactWrapper}>
+          <h4>Phone</h4>
+          <a href={userPhone}>{phone}</a>
+        </div>
 
-      <div className={s.contactBox}>
-        <p>Email</p>
-        <a href={userEmail}>{email}</a>
+        <div className={s.contactWrapper}>
+          <h4>Email</h4>
+          <a href={userEmail}>{email}</a>
+        </div>
       </div>
-
-      <Button>Send message</Button>
+      <Button accent maxSize>
+        Send message
+      </Button>
     </div>
   );
 };
